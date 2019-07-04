6d246dcbfea104d0af838ed0ea95a5a2a2487e2f
Leona Bätz <lbaetz@uos.de>
2019-07-04 10:59:43 +0200

    erperminet number inserted

--- 05_main.js
+++ 05_main.js
@@ -19,13 +19,13 @@ $("document").ready(function() {
 
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
+            experimentID: "72",
             serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            deployMethod: "directLink",
+            contact_email: "ldoppertin@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
